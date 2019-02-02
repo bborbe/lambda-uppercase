562b64ce26c139c6b71f215933a77c62f01747a1
Benjamin Borbe <bborbe@rocketnews.de>
2019-02-02 22:58:11 +0100

    add go mod

--- main.go
+++ main.go
@@ -3,19 +3,14 @@ package main
 import (
 	"context"
 	"log"
+	"strings"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type Input struct {
-}
-
-type Output struct {
-}
-
-func handler(ctx context.Context, input Input) (*Output, error) {
-
-	log.Printf("handle request")
-
-	return nil, nil
+func handler(ctx context.Context, input string) (string, error) {
+	log.Printf("handle requtest for %s", input)
+	return strings.ToUpper(input), nil
 }
 
 func main() {
